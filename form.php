8acba1f1f4670a5dc4a40f916e1c68b91e09505e
Julien <47785651+Dartagnan45@users.noreply.github.com>
2019-11-18 21:27:34 +0100

    Update form.php

--- form.php
+++ form.php
@@ -4,28 +4,32 @@ $_SESSION = $_POST;
 $errors = [];
 
 if (array_key_exists('submit', $_POST)) {
-    if (empty($_POST['firstname'])) {
+    if (isset($_POST['firstanme']) && empty($_POST['firstname'])) {
         $errors['firstname'] = "Le nom ne peut être vide";
     }
     if (!preg_match('#\w#', $_POST['firstname'])) {
         $errors['firstname'] = "Le nom doit contenir que des lettres";
     }
-    if (empty($_POST['lastname'])) {
+    if (isset($_POST['lastname']) && empty($_POST['lastname'])) {
         $errors['lastname'] = "Le nom ne peut être vide";
     }
     if (!preg_match('#\w#', $_POST['lastname'])) {
         $errors['lastname'] = "Le nom doit contenir que des lettres";
     }
-    if (empty($_POST['email']) OR !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    if (isset($_POST['email']) && empty($_POST['email']) OR !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         $errors['email'] = "L'email entré n'est pas valide";
+    }
+    if (isset($_POST['number']) && empty($_POST['number'])) {
+        $errors['number'] = "Le numéro ne peut être vide";
+        wordwrap($_POST['number'], 2, "-", true);
     }
     if (!preg_match('#(0|\+33)[1-9]([0-9]{2}){4}#', $_POST['number'])) {
         $errors['number'] = "Le numéro entré n'est pas valide";
     }
-    if (empty($_POST['sujet'])) {
+    if (isset($_POST['sujet']) && empty($_POST['sujet'])) {
         $errors['sujet'] = 'Veuillez selectionner un sujet';
     }
-    if (empty($_POST['message'])) {
+    if (isset($_POST['message']) && empty($_POST['message'])) {
         $errors['message'] = 'Le message ne peut être vide';
     }
     if (empty($errors)) {
@@ -50,61 +54,58 @@ if (array_key_exists('submit', $_POST)) {
     <title>Formulaire de contact</title>
 </head>
 <body>
-
-<div id="progress-inputs" class="progress">
-    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0"
-         aria-valuemin="0" aria-valuemax="100" style="width:0;">
-        <span id="sr-only">0%</span>
+<div class="progress">
+    <div id="progressbar" class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
+         style="width: 10%" aria-valuenow="100"
+         aria-valuemin="0" aria-valuemax="100">
     </div>
 </div>
 
 <div class="container">
-    <form id="input-progress" action="" method="post">
+    <form action="" method="post">
         <?php if (!empty($errors)): ?>
             <legend class="alert alert-danger">Des erreurs ont été trouvées</legend>
         <?php endif; ?>
         <h2><strong>Formulaire de contact</strong></h2>
-
         <div class="form-group">
-            <label for="firstname">Prénom :</label>
-            <input type="text" id="firstname" name="firstname" minlength="3" maxlength="12" class="moo">
+            <label for="prenom">Prénom :</label>
+            <input type="text" id="prenom" name="firstname" minlength="3" maxlength="12">
             <p><?php if (isset($errors['firstname'])) echo $errors['firstname'] ?></p>
         </div>
 
         <div class="form-group">
-            <label for="lastname">Nom :</label>
-            <input type="text" id="lastname" name="lastname" class="moo" required>
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="lastname">
             <p><?php if (isset($errors['lastname'])) echo $errors['lastname'] ?></p>
         </div>
 
         <div class="form-group">
-            <label for="email">Courriel :</label>
-            <input type="email" id="email" name="email" class="moo" required>
+            <label for="courriel">Courriel :</label>
+            <input type="email" id="courriel" name="email" required>
             <p><?php if (isset($errors['email'])) echo $errors['email'] ?></p>
         </div>
 
         <div class="form-group">
             <label for="number">Téléphone :</label>
-            <input placeholder="+33 (0)1-23-45-67-89" type="tel" id="number" name="number" class="moo" required>
-            <p><?php if (isset($errors['number'])) echo $errors['number'];
-                $_POST['number'] = wordwrap($_POST['number'], 2, "-", true); ?></p>
-        </div>
+            <input placeholder="+33 (0)1-23-45-67-89" type="tel" id="number" name="number" required>
+            <p><?php if (isset($errors['number'])) echo $errors['number']; ?></p>
+        </div> 
 
         <div class="form-group">
             <label for="message">Message :</label>
-            <textarea id="message" name="message" class="moo"></textarea>
+            <textarea id="message" name="message"></textarea>
             <p><?php if (isset($errors['message'])) echo $errors['message'] ?></p>
         </div>
 
         <div class="form-group">
-            <label for="subject">Sujet :</label>
-            <select id="subject" name="subject">
+            <label for="sujet">Sujet :</label>
+            <select id="sujet" name="sujet">
                 <option value="">Choisissez votre sujets</option>
                 <option value="probleme technique">probleme technique</option>
                 <option value="information">information</option>
                 <option value="conseil">conseil</option>
             </select>
-            <p><?php if (isset($errors['subject'])) echo $errors['subject'] ?></p>
+            <p><?php if (isset($errors['sujet'])) echo $errors['sujet'] ?></p>
         </div>
 
         <div class="button">
@@ -129,37 +130,25 @@ if (array_key_exists('submit', $_POST)) {
 
     setTimeout(function () {
         alert('Tu dors ?');
-    }, 100000);
+    }, 30000);
 
+    function progress(){
+        var progress = $('#progressbar').progressbar('option', 'value'); // récupération de la valeur actuelle
+        var value = $('input');
 
-    $(document).ready(function () {
-        function updateInputProgress() {
-            var filledFields = 0;
-            $("#input-progress").find("input, select, textarea").each(function () {
-                if ($(this).val() !== "") {
-                    filledFields++;
-                }
-            });
-            var percent = Math.ceil(100 * filledFields / totalFields);
-            $("#progress-inputs .progress-bar").attr("aria-valuenow", percent).width(percent + "%");
-            $("#sr-only").html(percent + "% Complete");
-
-            return percent;
+        if(progress < 100% && value > 0){
+            $('#progressbar').progressbar('option', 'value', progress + 20%); // on incrémente la valeur de 1 si elle est strictement inférieure à 100
+             // puis on relance la fonction
         }
+    }
 
-        //Input Progress
-        var totalFields = $("#input-progress").find("input, select, textarea").length;
-        $("#input-progress").click(function () {
-            updateInputProgress();
-        });
-        $("#input-progress .btn-primary").click(function () {
-            var percent = updateInputProgress();
-            if (percent === 100) {
-                alert("Finished inputs successfully!");
-            }
-        })
-
-    });
+    /*$(document).ready(function(){
+        var progressBarVal = 0 ;
+        var html="<div class='progress-bar progress-bar-striped active' role='progressbar' aria-valuenow="+progressBarVal+" " +
+            "aria-valuemin='0' aria-valuemax='100' style='width:"+progressBarVal+"%'>"+progressBarVal+"%</div>";
+        $(".progress").append(html);
+    });*/
+    // la fonction exécutera l'alerte après 5000 millisecondes, soit 5 secondes
 
 </script>
 </body>
